8cba28cde7e5c1375a84cf2ea20c921becb3e127
justjaxy <d0tanoob@hotmail.com>
2016-12-23 13:40:28 +0800

    Retrieve all trip function for driver references
    1_Developers: Sim Chee Kent, Lee Chan Xian
    2_Time: 12 Hours
    3_Use_case: #134994817, As a Driver, I want to have access to view all the travel location the company have even though there is no assignment on me so that I can have a overview of where the company capable of travelling.

--- TripDA.java
+++ TripDA.java
@@ -15,46 +15,7 @@ public class TripDA {
     private MaintainRouteControl mrc;
     private MaintainDriverlistControl msc;
 
-
-    
-     public ResultSet selectRecord(String id) {
-        String queryStr = "SELECT * FROM " + tableName + " WHERE tripid = ?";
-        ResultSet rs = null;
-        try {
-            stmt = conn.prepareStatement(queryStr);
-            stmt.setString(1, id);
-            rs = stmt.executeQuery();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "selectRecord, SQLException in TripDa " + ex.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
-        }
-        return rs;
-    }
-
-    public Trip getRecord(String ID) {
-        String queryStr = "SELECT * FROM " + tableName + " WHERE tripid = ?";
-        Trip trip = null;
-        try {
-            stmt = conn.prepareStatement(queryStr);
-            stmt.setString(1, ID);
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                Bus b = mbc.selectRecord(rs.getString(5));
-                Route r = mrc.selectRecord(rs.getString(6));
-                if (rs.getString(7) == null) {
-                    trip = new Trip(rs.getString(1), rs.getString(2), rs.getString(3), rs.getDouble(4), b, r, null);
-                } else {
-                    Driverlist s = msc.selectRecord(rs.getString(7));
-                    trip = new Trip(rs.getString(1), rs.getString(2), rs.getString(3), rs.getDouble(4), b, r, s);
-                }
-            }
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "getRecord, SQLException in TripDA " + ex.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
-        }
-        return trip;
-    }
-
- public ArrayList<Trip> getAllRecord() {
+    public ArrayList<Trip> getAllRecord() {
         ArrayList<Trip> tripList = new ArrayList<Trip>();
         String queryStr = "SELECT * FROM " + tableName;
         ResultSet rs = null;
@@ -70,63 +31,3 @@ public class TripDA {
         }
         return tripList;
     }
-
-public ArrayList<Trip> searchRecordByColumn(String tripName, String searchData) {
-        ArrayList<Trip> tripList = new ArrayList<Trip>();
-        String queryStr = "SELECT * FROM " + tableName + " where " + tripName + " LIKE '%" + searchData + "%'";
-        ResultSet rs = null;
-        try {
-            stmt = conn.prepareStatement(queryStr);
-            rs = stmt.executeQuery();
-            while (rs.next()) {
-                Trip trip = getRecord(rs.getString(1));
-                tripList.add(trip);
-            }
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "searchRecordByColumn, SQLException in TripDa " + ex.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
-        }
-        return tripList;
-    }
-
-    public ArrayList<String> getAllColumnName() {
-        ArrayList<String> s = new ArrayList<String>();
-        ResultSet rs = null;
-        try {
-            String queryStr = "SELECT * FROM " + tableName;
-            stmt = conn.prepareStatement(queryStr);
-            rs = stmt.executeQuery();
-            ResultSetMetaData rsmd = rs.getMetaData();
-            int columnCount = rsmd.getColumnCount();
-            for (int i = 1; i <= columnCount; i++) {
-                s.add(rsmd.getColumnName(i));
-            }
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "getAllColumnName, SQLException in TripDa " + ex.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
-        }
-        return s;
-    }
-
-    public Trip getRecord(String ID) {
-        String queryStr = "SELECT * FROM " + tableName + " WHERE tripid = ?";
-        Trip trip = null;
-        try {
-            stmt = conn.prepareStatement(queryStr);
-            stmt.setString(1, ID);
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                Bus b = mbc.selectRecord(rs.getString(5));
-                Route r = mrc.selectRecord(rs.getString(6));
-                if (rs.getString(7) == null) {
-                    trip = new Trip(rs.getString(1), rs.getString(2), rs.getString(3), rs.getDouble(4), b, r, null);
-                } else {
-                    Driverlist s = msc.selectRecord(rs.getString(7));
-                    trip = new Trip(rs.getString(1), rs.getString(2), rs.getString(3), rs.getDouble(4), b, r, s);
-                }
-            }
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "getRecord, SQLException in TripDA " + ex.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
-        }
-        return trip;
-    }
-
